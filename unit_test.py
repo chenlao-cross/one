825072d2b96b247f1ec2ecfdceb74f30d8fd7aa8
chenlao-cross <112846860+chenlao-cross@users.noreply.github.com>
2022-09-19 14:19:56 +0800

    Add files via upload

---
+++ unit_test.py
@@ -0,0 +1,12 @@
+import unittest
+from test import main_test
+#from main import main_test
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(main_test(),0.99)   #首先假设预测的是前面第一组运行的测试数据
+
+
+if __name__ == '__main__':
+    unittest.main()
+
